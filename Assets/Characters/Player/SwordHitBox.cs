5be2823951248aafe781ed1773e988a90820f3c2
Edleron <Edleron@hotmail.com>
2024-02-13 22:53:17 +0300

    fix :sword hit box

--- Assets/Characters/Player/SwordHitBox.cs
+++ Assets/Characters/Player/SwordHitBox.cs
@@ -21,7 +21,6 @@ public class SwordHitBox : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D col) {
         Debug.Log(col.gameObject.name);
-       
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
