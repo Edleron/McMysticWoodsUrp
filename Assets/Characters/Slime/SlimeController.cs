e3a23714e8dee34863c2add4f9725292dc10e4aa
Edleron <Edleron@hotmail.com>
2024-02-24 00:10:57 +0300

    fix : slime

--- Assets/Characters/Slime/SlimeController.cs
+++ Assets/Characters/Slime/SlimeController.cs
@@ -4,59 +4,16 @@ using UnityEngine;
 using Edleron;
 using System.Runtime.InteropServices.WindowsRuntime;
 
-public class SlimeController : MonoBehaviour, IDamagable
+public class SlimeController : MonoBehaviour
 {
-    private Animator animator;
-    private Collider2D col;
-    private Rigidbody2D rb;
+    private float damage        = 1;    
 
-    public float Healt {
-        get {
-            return _health;
+    private void OnCollisionEnter2D(Collision2D other) {
+        IDamagable damagable = other.collider.GetComponent<IDamagable>();
+
+        if (damagable != null)
+        {
+            damagable.OnHit(damage);
         }
-        set {
-            if (value < _health)
-            {
-                animator.SetTrigger("hit");
-            }
-
-            _health = value;            
-
-            if (_health <= 0)
-            {
-                // Destroy(gameObject);
-                isAlive = false;
-                Targetable = false;
-                animator.SetBool("isAlive", isAlive);
-            }
-        }
-    }
-
-    public bool Targetable { get { return _targetable;} set { _targetable = value; rb.simulated = value; col.enabled = false; } }
-
-    public float _health = 3;
-    public bool  _targetable = true;
-    private bool isAlive = true;
-
-    private void Start()
-    {
-        rb       = GetComponent<Rigidbody2D>();
-        col      = GetComponent<Collider2D>();
-        animator = GetComponent<Animator>();
-        animator.SetBool("isAlive", isAlive);
-    }
-
-    public void OnHit(float damage)
-    {
-        Debug.Log("Silme Hit For ! " + damage);
-        Healt -= damage;
-    }
-
-    public void OnHit(float damage, Vector2 knobcback)
-    {
-        Healt -= damage;
-        rb.AddForce(knobcback);
-
-        Debug.Log(knobcback);
     }
 }
