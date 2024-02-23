6808ee9e3358f89aedccb7fa89e0a53f2346765c
Edleron <Edleron@hotmail.com>
2024-02-24 00:11:15 +0300

    fix : player

--- Assets/Characters/Player/SwordHitBox.cs
+++ Assets/Characters/Player/SwordHitBox.cs
@@ -8,10 +8,10 @@ using UnityEngine;
 public class SwordHitBox : MonoBehaviour
 {
     public float swordDamage = 1.0f;
-    public float knockbackForce = 7500f;
+    public float knockbackForce = 10f;
     public Collider2D swordCollider;    
-    public Vector3 faceRight = new Vector3(1, -0.9f, 0);
-    public Vector3 faceLeft  = new Vector3(-1, -0.9f, 0);
+    public Vector3 faceRight = new Vector3(1, 0.25f, 0);
+    public Vector3 faceLeft  = new Vector3(-1, 0.25f, 0);
 
     private void Start ()
     {
@@ -23,18 +23,14 @@ public class SwordHitBox : MonoBehaviour
         // swordCollider = GetComponent<Collider2D>();
     }
 
-    private void OnCollisionEnter2D(Collision2D col) {
-        Debug.Log(col.gameObject.name);
-    }
-
     private void OnTriggerEnter2D(Collider2D other) {
         
         IDamagable damagableObject = other.GetComponent<IDamagable>();
 
         if (damagableObject != null)
         {
-            Vector3 parentPosition = gameObject.GetComponentInParent<Transform>().position;
-            Vector2 direction      = (Vector2) (other.gameObject.transform.position - parentPosition).normalized;
+            Vector3 parentPosition = transform.parent.position;
+            Vector2 direction      = (other.transform.position - parentPosition).normalized;
             Vector2 knokcback      = direction * knockbackForce;
 
             // other.SendMessage("OnHit", swordDamage, knocback);   
